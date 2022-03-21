a8daeee2e1f290f28298034586c3bc54ae4a76d9
peripheralist <peripheralist@gmail.com>
2022-03-21 18:17:16 -0400

    -

--- src/erc20/v2/mapping.ts
+++ src/erc20/v2/mapping.ts
@@ -3,7 +3,7 @@ import { BigInt, dataSource } from "@graphprotocol/graph-ts";
 import { Transfer } from "../../../generated/templates/ERC20/ERC20";
 import { handleProjectERC20Transfer } from "../../utils";
 
-export function handleV2Transfer(event: Transfer) {
+export function handleV2Transfer(event: Transfer): void {
   let context = dataSource.context();
   let projectId = context.getString("projectId");
 
