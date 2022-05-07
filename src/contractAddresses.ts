d1447b5b35c1d967c88859d1133d12a0ae06d6c7
peripheralist <peripheralist@gmail.com>
2022-05-07 15:01:39 -0400

    update abis + addresses

--- src/contractAddresses.ts
+++ src/contractAddresses.ts
@@ -3,8 +3,8 @@ export const address_terminalV1 = "0xd569D3CCE55b71a8a3f3C418c329A66e5f714431";
 export const address_terminalV1_1 = "0x981c8ECD009E3E84eE1fF99266BF1461a12e5c68";
 export const address_terminalDirectory = "0x46C9999A2EDCD5aA177ed7E8af90c68b7d75Ba46";
 export const address_ticketBooth = "0xee2eBCcB7CDb34a8A822b589F9E8427C24351bfc";
-export const address_jbProjects = "0x892a6159e6EB452B270eeDd9A46BE660E0Fe8EC1";
-export const address_jbController = "0x53C0CE78b61B29816b548C705dAc4cD4839693b6";
-export const address_jbETHPaymentTerminal = "0x6de07872A1732040d26506d84F8B4f0D4869742e";
-export const address_jbTokenStore = "0x6B0bD488c2122Ad61513437f2e0619d53d128895";
+export const address_jbProjects = "0x2109e1CaF001980C318Ad7efdE8Fc204a844273b";
+export const address_jbController = "0x43780E780DF2bD1e4d955d3d4b577a490841241A";
+export const address_jbETHPaymentTerminal = "0x2109e1CaF001980C318Ad7efdE8Fc204a844273b";
+export const address_jbTokenStore = "0x9c54a10a35bF8cC8bF4AE52198c782c5681c9470";
 
