f5daf07305162bb9f1c07ca779c3d564cb4a29dd
peripheralist <peripheralist@gmail.com>
2022-05-28 21:23:44 -0400

    update contract addresses

--- src/contractAddresses.ts
+++ src/contractAddresses.ts
@@ -4,7 +4,7 @@ export const address_terminalV1_1 = "0xE7432E80A75F957121Cb523BA842Db27C6C25F69"
 export const address_terminalDirectory = "0x88d8c9E98E6EdE75252c2473abc9724965fe7474";
 export const address_ticketBooth = "0x0d038636a670E8bd8cF7D56BC4626f2a6446cF11";
 export const address_jbProjects = "0x2d8e361f8F1B5daF33fDb2C99971b33503E60EEE";
-export const address_jbController = "0x253a03a5D03c8a9AdF4D428b7e1Ef2800Cb62e78";
-export const address_jbETHPaymentTerminal = "0x8EcC95ba7da90b94C3cd56A92BEe3cDE6b132d5c";
-export const address_jbTokenStore = "0x79F0F3dE9cDbf7201C18826D712ba91CF3e735Cf";
+export const address_jbController = "0x418d8199F96F7c65b5b716f8E2c6327dbA266B26";
+export const address_jbETHPaymentTerminal = "0xe59707E9413e5FB6905017981B2815940e87e5fd";
+export const address_jbTokenStore = "0x1c4bC800c303cEB202944eDdB1cE8e88357284b8";
 
