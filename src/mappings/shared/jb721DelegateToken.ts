579925819e54f272023c8504ef781d0c3d99ce78
peripheralist <peripheralist@gmail.com>
2022-10-24 13:55:03 -0500

    add 721 tier info

--- src/mappings/shared/jb721DelegateToken.ts
+++ src/mappings/shared/jb721DelegateToken.ts
@@ -1,8 +1,12 @@
-import { Address, dataSource, log } from "@graphprotocol/graph-ts";
+import { Address, Bytes, dataSource, log } from "@graphprotocol/graph-ts";
 
 import { JB721DelegateToken, Participant } from "../../../generated/schema";
 import { Transfer } from "../../../generated/templates/ERC20/ERC20";
 import { ERC721 } from "../../../generated/templates/JB721DelegateToken/ERC721";
+import { JBTiered721DelegateStore } from "../../../generated/templates/JB721DelegateToken/JBTiered721DelegateStore";
+import {
+  address_v3_jbTiered721DelegateStore,
+} from "../../contractAddresses";
 import { newParticipant } from "../../utils/entity";
 import {
   idForJB721DelegateToken,
@@ -15,30 +19,35 @@ export function handleTransfer(event: Transfer): void {
   const projectId = context.getBigInt("projectId");
   const pv = context.getString("pv");
   const address = dataSource.address();
-  const contract = ERC721.bind(Address.fromBytes(address));
+  const jbTiered721DelegateContract = ERC721.bind(Address.fromBytes(address));
 
-  const id = idForJB721DelegateToken(
-    Address.fromBytes(address),
-    event.params.value
-  );
+  const tokenId = event.params.value;
+
+  const id = idForJB721DelegateToken(Address.fromBytes(address), tokenId);
 
   let token = JB721DelegateToken.load(id);
 
+  /**
+   * If entity doesn't exist, we create and get the values that aren't expected to change.
+   */
   if (!token) {
     // Create entity
     token = new JB721DelegateToken(id);
+    token.tokenId = tokenId;
+    token.address = address;
+    token.projectId = projectId.toI32();
+    token.project = idForProject(projectId, pv);
 
-    /**
-     * When creating the token entity, we only get the values that we don't expect to change.
-     */
-    const nameCall = contract.try_name();
+    // Name
+    const nameCall = jbTiered721DelegateContract.try_name();
     if (nameCall.reverted) {
       log.error("[handleTransfer] name() reverted for jb721Delegate:{}", [id]);
       return;
     }
     token.name = nameCall.value;
 
-    const symbolCall = contract.try_symbol();
+    // Symbol
+    const symbolCall = jbTiered721DelegateContract.try_symbol();
     if (symbolCall.reverted) {
       log.error("[handleTransfer] symbol() reverted for jb721Delegate:{}", [
         id,
@@ -46,17 +55,42 @@ export function handleTransfer(event: Transfer): void {
       return;
     }
     token.symbol = symbolCall.value;
-    token.tokenId = event.params.value;
-    token.address = address;
-    token.projectId = projectId.toI32();
-    token.project = idForProject(projectId, pv);
+
+    // Tier data
+    if (!address_v3_jbTiered721DelegateStore) {
+      log.error(
+        "[handleTransfer] missing address_v3_jbTiered721DelegateStore",
+        []
+      );
+      return;
+    }
+    const jbTiered721DelegateStoreContract = JBTiered721DelegateStore.bind(
+      Address.fromBytes(
+        Bytes.fromHexString(address_v3_jbTiered721DelegateStore!)
+      )
+    );
+    const tierCall = jbTiered721DelegateStoreContract.try_tier(
+      address,
+      tokenId
+    );
+    if (tierCall.reverted) {
+      log.error("[handleTransfer] tier() reverted for address {}, tokenId {}", [
+        address.toHexString(),
+        tokenId.toString(),
+      ]);
+      return;
+    }
+    token.floorPrice = tierCall.value.contributionFloor;
+    token.lockedUntil = tierCall.value.lockedUntil;
   }
 
   /**
    * Some params may change, so we update them every time the token
    * is transferred.
    */
-  const tokenUriCall = contract.try_tokenURI(event.params.value);
+  const tokenUriCall = jbTiered721DelegateContract.try_tokenURI(
+    event.params.value
+  );
   if (tokenUriCall.reverted) {
     log.error("[handleTransfer] tokenURI() reverted for jb721Delegate:{}", [
       id,
