d2e079b0b4ff82b47bd2c43b385f039c15e06999
peripheralist <peripheralist@gmail.com>
2023-08-21 13:02:50 -0700

    add support for more jb721Delegates. new Collection entity pattern

--- src/mappings/shared/jbTiered721DelegateDeployer3.ts
+++ src/mappings/shared/jbTiered721DelegateDeployer3.ts
@@ -1,8 +1,11 @@
-import { DataSourceContext } from "@graphprotocol/graph-ts";
+import { Address, DataSourceContext, log } from "@graphprotocol/graph-ts";
 
 import { DelegateDeployed } from "../../../generated/JBTiered721DelegateDeployer3/JBTiered721DelegateDeployer3";
-import { JB721Delegate3 } from "../../../generated/templates";
+import { JB721Delegate3 as JB721Delegate3DataSource } from "../../../generated/templates";
+import { JB721Delegate3 } from "../../../generated/templates/JB721Delegate3/JB721Delegate3";
 import { PV } from "../../enums";
+import { idForProject } from "../../utils/ids";
+import { JB721DelegateCollection } from "../../../generated/schema";
 
 const pv = PV.PV2;
 
@@ -16,9 +19,41 @@ export function handleDelegateDeployed(event: DelegateDeployed): void {
   const jbTiered721DelegateContext = new DataSourceContext();
   jbTiered721DelegateContext.setBigInt("projectId", event.params.projectId);
   jbTiered721DelegateContext.setString("pv", pv.toString());
-  jbTiered721DelegateContext.setI32(
-    "governanceType",
-    event.params.governanceType
+
+  // Create a data source that will track token transfers
+  JB721Delegate3DataSource.createWithContext(
+    address,
+    jbTiered721DelegateContext
   );
-  JB721Delegate3.createWithContext(address, jbTiered721DelegateContext);
+
+  const collection = new JB721DelegateCollection(address.toHexString());
+  collection.projectId = event.params.projectId.toI32();
+  collection.governanceType = event.params.governanceType;
+  collection.project = idForProject(event.params.projectId, pv);
+
+  const jb721DelegateContract = JB721Delegate3.bind(Address.fromBytes(address));
+
+  // Name
+  const nameCall = jb721DelegateContract.try_name();
+  if (nameCall.reverted) {
+    log.error(
+      "[jb721_v1:handleTransfer] name() reverted for jb721Delegate:{}",
+      [address.toHexString()]
+    );
+    return;
+  }
+  collection.name = nameCall.value;
+
+  // Symbol
+  const symbolCall = jb721DelegateContract.try_symbol();
+  if (symbolCall.reverted) {
+    log.error(
+      "[jb721_v1:handleTransfer] symbol() reverted for jb721Delegate:{}",
+      [address.toHexString()]
+    );
+    return;
+  }
+  collection.symbol = symbolCall.value;
+
+  collection.save();
 }
--- src/mappings/shared/jbTiered721DelegateDeployer3_1.ts
+++ src/mappings/shared/jbTiered721DelegateDeployer3_1.ts
@@ -1,8 +1,11 @@
-import { DataSourceContext } from "@graphprotocol/graph-ts";
+import { Address, DataSourceContext, log } from "@graphprotocol/graph-ts";
 
 import { DelegateDeployed } from "../../../generated/JBTiered721DelegateDeployer3_1/JBTiered721DelegateDeployer3_1";
-import { JB721Delegate3_1 } from "../../../generated/templates";
+import { JB721Delegate3_1 as JB721Delegate3_1DataSource } from "../../../generated/templates";
+import { JB721Delegate3_1 } from "../../../generated/templates/JB721Delegate3_1/JB721Delegate3_1";
 import { PV } from "../../enums";
+import { JB721DelegateCollection } from "../../../generated/schema";
+import { idForProject } from "../../utils/ids";
 
 const pv = PV.PV2;
 
@@ -16,9 +19,43 @@ export function handleDelegateDeployed(event: DelegateDeployed): void {
   const jbTiered721DelegateContext = new DataSourceContext();
   jbTiered721DelegateContext.setBigInt("projectId", event.params.projectId);
   jbTiered721DelegateContext.setString("pv", pv.toString());
-  jbTiered721DelegateContext.setI32(
-    "governanceType",
-    event.params.governanceType
+
+  // Create a data source that will track token transfers
+  JB721Delegate3_1DataSource.createWithContext(
+    address,
+    jbTiered721DelegateContext
   );
-  JB721Delegate3_1.createWithContext(address, jbTiered721DelegateContext);
+
+  const collection = new JB721DelegateCollection(address.toHexString());
+  collection.projectId = event.params.projectId.toI32();
+  collection.governanceType = event.params.governanceType;
+  collection.project = idForProject(event.params.projectId, pv);
+
+  const jb721DelegateContract = JB721Delegate3_1.bind(
+    Address.fromBytes(address)
+  );
+
+  // Name
+  const nameCall = jb721DelegateContract.try_name();
+  if (nameCall.reverted) {
+    log.error(
+      "[jb721_v1:handleTransfer] name() reverted for jb721Delegate:{}",
+      [address.toHexString()]
+    );
+    return;
+  }
+  collection.name = nameCall.value;
+
+  // Symbol
+  const symbolCall = jb721DelegateContract.try_symbol();
+  if (symbolCall.reverted) {
+    log.error(
+      "[jb721_v1:handleTransfer] symbol() reverted for jb721Delegate:{}",
+      [address.toHexString()]
+    );
+    return;
+  }
+  collection.symbol = symbolCall.value;
+
+  collection.save();
 }
