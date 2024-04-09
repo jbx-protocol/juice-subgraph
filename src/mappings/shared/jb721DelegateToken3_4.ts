6eab3e7969466bade170c1249450b85fdefa2a1d
peripheralist <peripheralist@gmail.com>
2024-04-09 16:38:31 -0600

    random log

--- src/mappings/shared/jb721DelegateToken3_4.ts
+++ src/mappings/shared/jb721DelegateToken3_4.ts
@@ -167,6 +167,7 @@ export function handleAddTier(event: AddTier): void {
   );
   if (tierCall.reverted) {
     // Will revert for non-tiered tokens, among maybe other reasons
+    // Logged on 3/3/24 v8.1.7: ERRO [jb721_v3_4:handleTransfer] tierOf() reverted for address 0xa8e6d676895b0690751ab1eaee09e15a3905d1b5, tierId 2, data_source: JB721Delegate3_4, sgd: 2599, subgraph_id: QmNT7qKcjCnvnPt7xNUr1azCkNBC64hrupuL1maedavFT1, component: SubgraphInstanceManager > UserMapping
     log.error(
       "[jb721_v3_4:handleTransfer] tierOf() reverted for address {}, tierId {}",
       [address.toHexString(), tierId.toString()]
